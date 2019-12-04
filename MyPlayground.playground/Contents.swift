6cc2285bab89f31f7ea30fd47f1d3cee0ea146ad
Ilya Makarevich <ilya.makarevich@apalon.com>
2019-12-04 13:24:08 +0300

    add projects

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+
+        //change heigh and width
+        
+        label.frame = CGRect(x: 150, y: 200, width: 5, height: 3)
+
+        
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
