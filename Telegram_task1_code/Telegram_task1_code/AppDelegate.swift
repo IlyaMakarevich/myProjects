1699b51cc360f50cb406a5c09bd8a62603132887
MacBook Pro Home <makarevi4@gmail.com>
2019-12-19 23:16:50 +0300

    create Twitter project

--- Telegram_task1_code/Telegram_task1_code/AppDelegate.swift
+++ Telegram_task1_code/Telegram_task1_code/AppDelegate.swift
@@ -15,13 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         let mainVC = MainTabViewController()
         window?.rootViewController = mainVC
-
-
         return true
     }
 
---
+++ twitter/twitter/UserViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  twitter
+//
+//  Created by MacBook Pro  on 19.12.2019.
+//  Copyright © 2019 MacBook Pro . All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVC()
+    }
+    
+    func setupVC() {
+        view.backgroundColor = .systemBlue
+        navigationItem.title = "Home"
+    }
+
+}
