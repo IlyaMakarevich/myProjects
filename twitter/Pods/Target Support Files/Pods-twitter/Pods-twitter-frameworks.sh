e7670f26d85fab0e4238a93a928985ca8b5f4a2d
MacBook Pro Home <makarevi4@gmail.com>
2019-12-21 17:28:55 +0300

    add alamofire pod

--- twitter/Pods/Target Support Files/Pods-twitter/Pods-twitter-frameworks.sh
+++ twitter/Pods/Target Support Files/Pods-twitter/Pods-twitter-frameworks.sh
@@ -161,16 +161,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
-  install_framework "${PODS_ROOT}/TwitterCore/iOS/TwitterCore.framework"
-  install_framework "${PODS_ROOT}/TwitterKit/iOS/TwitterKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
-  install_framework "${PODS_ROOT}/TwitterCore/iOS/TwitterCore.framework"
-  install_framework "${PODS_ROOT}/TwitterKit/iOS/TwitterKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
